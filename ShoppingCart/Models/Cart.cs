a9d83eb91ebefd258797d91182e221003b509a3f
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-03-25 19:35:46 +0000

    New classs for Cart added to handle adding items

---
+++ ShoppingCart/Models/Cart.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Cart
+    {
+        //Adding to a list for each item in the cart
+        //We do not want an entry for each item - one entry for an item with the qty included
+        //e.g. 2 oranges would be 1 entry not two seperate entries
+        private List<CartItem> items;
+
+        public Cart()
+        {
+            items = new List<CartItem>();
+        }
+
+        public void AddItem(Item choice)
+        {
+            //Check that the item is not in the list already
+            //ToUpperInvarient means it will look for upper and lower case
+            CartItem found = items.FirstOrDefault(p => p.Code.ToUpperInvariant() == choice.Code.ToUpperInvariant());
+            if(found != null)
+            {
+                found.Qty++;
+            }
+            else
+            {
+                items.Add(new CartItem() { Code = choice.Code, Description = choice.Description, Price = choice.Price, Qty = 1 });
+            }
+
+        }
+
+    }
+}
