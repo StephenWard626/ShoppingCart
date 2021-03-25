640e6cf38d05c1212155d49dce134beca74f16ca
StephenWard626 <78171486+StephenWard626@users.noreply.github.com>
2021-03-25 19:12:26 +0000

    Model Class added

---
+++ ShoppingCart/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Models
+{
+    public class Cart
+    {
+        public string ProductCode { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public class 
+}
